627048cf279fe3ccdc9ebf68bec560c13badcf8a
dresanchez-sammy <andreasesanchez17@gmail.com>
2019-11-26 08:07:39 -0800

    Updates. Need to fix dependencies/build issues.

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,17 +1,18 @@
+import java.awt.Desktop;
 import java.io.*;
+import java.net.URI;
 import java.nio.file.*;
-import java.sql.DriverManager;
 import java.util.*;
 import java.util.stream.*;
 
+import javax.servlet.http.*;
 
-
-import java.lang.*;
 import java.sql.*;
 
 
 import com.mpatric.mp3agic.*;
-import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.*;
 
 /**
  * This application takes in the directory path that holds mp3s
@@ -24,14 +25,14 @@ public class Main {
 		/*Step 1.
 		 * Validate input from user.
 		 */
-/*		if (args.length != 1) {
+		/*		if (args.length != 1) {
 			throw new IllegalArgumentException("Specify a valid directory.");
 		}
-*/
+		 */
 		//Path path = Paths.get(args[0]); // Create Path object
 		//Having trouble running from commandline. 
 		Path path = Paths.get("C:\\Users\\asanchez\\Desktop\\mp3s");
-		
+
 		if (!Files.exists(path)) { // Verify path actually exists.
 			throw new IllegalArgumentException("Specifed directory does not exist: " + path);
 		}
@@ -50,7 +51,7 @@ public class Main {
 				filePaths.add(p);
 			});
 		}
-		
+
 		//Step 3: Part 1. Using streams to construct list of Song objects created from MP3 data.
 		List<Song> songs = filePaths.stream().map(p -> {
 			try {
@@ -62,12 +63,12 @@ public class Main {
 				throw new IllegalStateException(e);
 			}
 		}).collect(Collectors.toList());
-		
+
 		//Step 4: Database. 
 		// Can probably update this to Derby on home computer.
 		try (Connection conn = DriverManager.getConnection("jdbc:h2:~/mydatabase;AUTO_SERVER=TRUE;INIT=runscript from './create.sql")) {
 			PreparedStatement ps = conn.prepareStatement("INSERT INTO Songs (artist, year, album, title) VALUES (?, ?, ?, ?)");
-			
+
 			for(Song s : songs) {
 				ps.setString(1, s.getArtist());
 				ps.setString(2, s.getYear());
@@ -75,16 +76,68 @@ public class Main {
 				ps.setString(4, s.getTitle());
 				ps.addBatch();
 			}
-			
+
 			int[] rows = ps.executeBatch();
 			System.out.println("Number of rows inserted: " + Arrays.toString(rows));			
 		}
-		
+
 		//Step 5: Start HTTP Server - Part 1
-		// This is pretty new for us.. Going to embed Jetty into Java program.
+		//This is pretty new for us.. Going to embed Jetty into Java program.
 		Server server = new Server(8080);
+
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		context.setResourceBase(System.getProperty("java.io.tmpdir"));
+		server.setHandler(context);
 		
+		context.addServlet(SongServlet.class, "/songs");
+		server.start();
+		
+		if (Desktop.isDesktopSupported()) {
+			Desktop.getDesktop().browse(new URI("http://localhost:8080/songs"));
+		}
+		
+		
+
 	}
+	
+	//Step 5 : Write a servlet - Part 2
+	
+	public static class SongServlet extends HttpServlet {
+		
+		//Method which responds to HTTP GET calls (from the browser) on the path your servlet is registered to.
+		protected void doGet(HttpServlet req, HttpServletResponse resp ) throws IOException {
+			StringBuilder builder = new StringBuilder();
+			
+			try (Connection conn = DriverManager.getConnection("jdbc:h2:~/mydatabase")){
+				Statement stat = conn.createStatement();
+				ResultSet rs = stat.executeQuery("SELECT * FROM Songs");
+				
+				while(rs.next()) {
+					builder.append("<tr class=\"table\">")
+							.append("<td>").append(rs.getString("year")).append("</td>")
+							.append("<td>").append(rs.getString("artist")).append("</td>")
+							.append("<td>").append(rs.getString("album")).append("</td>")
+							.append("<td>").append(rs.getString("title")).append("</td>")
+							.append("</tr>");
+							
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String string = "<html><h1>Yours Songs</h1><table><tr><th>Year</th><th>Artist</th><th>Album</th><th>Title</th></tr>" + builder.toString() + "</table></html>";
+			resp.getWriter().write(string);
+		}
+	}
+
+
+	public static Connection getConnection() {
+
+
+		return null; // DriverManager.getConnection();
+	}
+
 	/**
 	 * Step 3: Part 2
 	 * Class to represent a Song.
